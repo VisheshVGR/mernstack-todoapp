f2454769289c6e29ddfe98a2328eb4457ecfdc97
Vishesh Gupta <42836833+VisheshVGR@users.noreply.github.com>
2023-03-01 19:25:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ app.use('/api/user', userController)
 // Showing build file
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("./frontend/build"))
+    
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
 }
 
 
